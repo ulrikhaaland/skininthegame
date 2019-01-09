7b8fbc3c1f5cd7df99f1d654ac913982820b0718
Ulrik Haaland <ulrikhaaland2@gmail.com>
2019-01-09 18:24:43 +0100

    graphs works

--- lib/pages/results/graph.dart
+++ lib/pages/results/graph.dart
@@ -5,6 +5,8 @@ import 'dart:async';
 import 'package:yadda/objects/resultgame.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:yadda/pages/results/results_graph_page.dart';
+import 'results.dart';
+import 'package:yadda/utils/uidata.dart';
 
 class Blabla extends StatefulWidget {
   final User user;
@@ -13,19 +15,21 @@ class Blabla extends StatefulWidget {
   _BlablaState createState() => _BlablaState();
 }
 
-class _BlablaState extends State<Blabla> {
+class _BlablaState extends State<Blabla> with TickerProviderStateMixin {
+  TabController _tabController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    tournament = new List<ResultGame>();
-    cashgame = new List<ResultGame>();
+    tournament = new List();
+    _tabController = new TabController(vsync: this, length: 2);
 
     getData();
   }
 
+  charts.Series<ResultGame, DateTime> data;
   List<ResultGame> tournament;
-  List<ResultGame> cashgame;
 
   void getData() async {
     QuerySnapshot qSnap = await Firestore.instance
@@ -35,6 +39,17 @@ class _BlablaState extends State<Blabla> {
       ResultGame resultGame = ResultGame.fromMap(doc.data);
       tournament.add(resultGame);
     });
+    tournament.sort((a, b) => a.date.compareTo(b.date));
+    data = charts.Series<ResultGame, DateTime>(
+      colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+      id: 'US Sales',
+      domainFn: (ResultGame game, _) => game.date,
+      measureFn: (ResultGame game, _) => int.tryParse(game.profit),
+      data: tournament,
+
+      // fillColorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+    );
+
     for (int i = 0; i < tournament.length; i++) {
       List<ResultGame> tournament1 = new List();
       tournament1.addAll(tournament);
@@ -51,13 +66,13 @@ class _BlablaState extends State<Blabla> {
         }
       }
     }
-    QuerySnapshot qSnap2 = await Firestore.instance
-        .collection("users/${widget.user.id}/cashgameresults")
-        .getDocuments();
-    qSnap2.documents.forEach((DocumentSnapshot doc) {
-      ResultGame resultGame = ResultGame.fromMap(doc.data);
-      cashgame.add(resultGame);
-    });
+    // QuerySnapshot qSnap2 = await Firestore.instance
+    //     .collection("users/${widget.user.id}/cashgameresults")
+    //     .getDocuments();
+    // qSnap2.documents.forEach((DocumentSnapshot doc) {
+    //   ResultGame resultGame = ResultGame.fromMap(doc.data);
+    //   cashgame.add(resultGame);
+    // });
     // chart = new charts.Series<TimeSeriesSales, DateTime>(
     //   id: 'UK Sales',
     //   domainFn: (TimeSeriesSales sales, _) => data[0].time,
@@ -75,19 +90,53 @@ class _BlablaState extends State<Blabla> {
   Widget build(BuildContext context) {
     if (ready)
       return Scaffold(
-        backgroundColor: Colors.white,
-        body:
-            // new Container()
-            SelectionCallbackExample.withSampleData(tournament),
-
-        // charts.Series<TimeSeriesSales, DateTime>(
-        //   id: 'US Sales',
-        //   domainFn: (TimeSeriesSales sales, _) => sales.time,
-        //   // measureFn: (TimeSeriesSales sales, _) => sales.sales,
-        //   data: data,
-        // ),
+        appBar: AppBar(
+          title: new Text(
+            "Results",
+            style: new TextStyle(
+                color: UIData.blackOrWhite, fontSize: UIData.fontSize24),
+          ),
+          backgroundColor: UIData.appBarColor,
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(
+                child: Text(
+                  "Cash Games",
+                  style: new TextStyle(color: UIData.blackOrWhite),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Tournaments",
+                  style: new TextStyle(color: UIData.blackOrWhite),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: new Stack(children: <Widget>[
+          new TabBarView(
+            physics: ScrollPhysics(),
+            controller: _tabController,
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 12),
+                      
+                    ),
+                    SelectionCallbackExample.withSampleData(data, widget.user)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ]),
+        backgroundColor: UIData.dark,
       );
     else
-      return Scaffold(backgroundColor: Colors.white, body: new Container());
+      return Scaffold(backgroundColor: UIData.dark, body: new Container());
   }
 }
--- lib/pages/results/results_graph_page.dart
+++ lib/pages/results/results_graph_page.dart
@@ -16,19 +16,24 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:yadda/objects/user.dart';
 import 'package:yadda/objects/resultgame.dart';
+import 'package:yadda/utils/uidata.dart';
+import 'package:yadda/pages/profile/profile_page_results.dart';
 
 class SelectionCallbackExample extends StatefulWidget {
   final List<charts.Series> seriesList;
   final bool animate;
+  final User user;
 
-  SelectionCallbackExample(this.seriesList, {this.animate});
+  SelectionCallbackExample(this.seriesList, {this.animate, this.user});
 
   /// Creates a [charts.TimeSeriesChart] with sample data and no transition.
-  factory SelectionCallbackExample.withSampleData(List<ResultGame> tournament) {
+  factory SelectionCallbackExample.withSampleData(
+      charts.Series<ResultGame, DateTime> tournament, User user) {
     return new SelectionCallbackExample(
       _createSampleData(tournament),
       // Disable animations for image tests.
-      animate: false,
+      animate: true,
+      user: user,
     );
   }
 
@@ -39,12 +44,10 @@ class SelectionCallbackExample extends StatefulWidget {
 
   /// Create one series with sample hard coded data.
   static List<charts.Series<ResultGame, DateTime>> _createSampleData(
-      List<ResultGame> tournament) {
+      charts.Series<ResultGame, DateTime> tournament) {
     // final tournament = [
-    //   new TimeSeriesSales(new DateTime(2017, 9, 19), 5),
-    //   new TimeSeriesSales(new DateTime(2017, 9, 26), 25),
-    //   new TimeSeriesSales(new DateTime(2017, 10, 3), 78),
-    //   new TimeSeriesSales(new DateTime(2017, 10, 10), 54),
+    //   tournament1.data[0].date,
+    //   tournament1.data[0],
     // ];
 
     // final cashgame = [
@@ -55,12 +58,13 @@ class SelectionCallbackExample extends StatefulWidget {
     // ];
 
     return [
-      new charts.Series<ResultGame, DateTime>(
-        id: 'US Sales',
-        domainFn: (ResultGame game, _) => game.date,
-        measureFn: (ResultGame game, _) => int.tryParse(game.profit),
-        data: tournament,
-      ),
+      tournament
+      // new charts.Series<ResultGame, DateTime>(
+      //   id: 'US Sales',
+      //   domainFn: (ResultGame game, _) => game.date,
+      //   measureFn: (ResultGame game, _) => int.tryParse(game.profit),
+      //   data: tournament,
+      // ),
       // new charts.Series<ResultGame, DateTime>(
       //   id: ' Sales',
       //   domainFn: (ResultGame game, _) => game.date,
@@ -74,6 +78,16 @@ class SelectionCallbackExample extends StatefulWidget {
 class _SelectionCallbackState extends State<SelectionCallbackExample> {
   DateTime _time;
   Map<int, ResultGame> _measures;
+  var color = charts.MaterialPalette.white;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.user.nightMode != true) {
+      color = charts.MaterialPalette.black;
+    }
+  }
 
   // Mapens to the underlying selection changes, and updates the information
   // relevant to building the primitive legend like information under the
@@ -105,41 +119,119 @@ class _SelectionCallbackState extends State<SelectionCallbackExample> {
 
   @override
   Widget build(BuildContext context) {
-    // The children consist of a Chart and Text widgets below to hold the info.
+    Size deviceSize = MediaQuery.of(context).size;
     final children = <Widget>[
       new SizedBox(
-          height: 550.0,
-          child: new charts.TimeSeriesChart(
-            widget.seriesList,
-            animate: widget.animate,
-            
-            selectionModels: [
-              new charts.SelectionModelConfig(
-                type: charts.SelectionModelType.info,
-                changedListener: _onSelectionChanged,
-              )
-            ],
-          )),
+          height: deviceSize.height / 2,
+          child: new Padding(
+              padding: EdgeInsets.all(3),
+              child: new charts.TimeSeriesChart(
+                widget.seriesList,
+                animate: widget.animate,
+                primaryMeasureAxis: charts.NumericAxisSpec(
+                    renderSpec: charts.GridlineRendererSpec(
+                        labelStyle:
+                            charts.TextStyleSpec(fontSize: 12, color: color),
+                        lineStyle: charts.LineStyleSpec(
+                            thickness: 1,
+                            color: charts.MaterialPalette.gray.shadeDefault))),
+                domainAxis: new charts.DateTimeAxisSpec(
+                  renderSpec: charts.SmallTickRendererSpec(
+                      axisLineStyle: charts.LineStyleSpec(
+                        color: color,
+                      ),
+                      labelStyle: new charts.TextStyleSpec(
+                        fontSize: 12,
+                        color: color,
+                      ),
+                      lineStyle:
+                          charts.LineStyleSpec(thickness: 1, color: color)),
+                ),
+                selectionModels: [
+                  new charts.SelectionModelConfig(
+                    type: charts.SelectionModelType.info,
+                    changedListener: _onSelectionChanged,
+                  )
+                ],
+              ))),
     ];
 
-    // If there is a selection, then include the details.
-    if (_time != null) {
-      children.add(new Padding(
-          padding: new EdgeInsets.only(top: 5.0),
-          child: new Text(_time.toString())));
-    }
     _measures?.forEach((int, ResultGame game) {
-      children.add(new Text('${game.profit}: 20'));
+      children.add(tournamentList(game));
     });
 
     return new Column(children: children);
   }
-}
 
-/// Sample time series data type.
-class TimeSeriesSales {
-  final DateTime time;
-  final ResultGame sales;
+  Widget tournamentList(ResultGame game) {
+    Color color;
+    String name = game.groupName;
+    String currency = game.currency;
+    String profit = game.profit;
 
-  TimeSeriesSales(this.time, this.sales);
+    if (int.tryParse(game.profit).isNegative) {
+      color = Colors.red;
+    } else {
+      color = Colors.green;
+    }
+    if (name.length > 13) {
+      name = name.substring(0, 10);
+      name = name + "...";
+    }
+    if (currency.length >= 6) {
+      currency = currency.substring(0, 3);
+      currency = currency + "...";
+    }
+    if (profit.length >= 11) {
+      profit = profit.substring(0, 9);
+      profit = profit + "...";
+    }
+    return ListTile(
+      contentPadding: EdgeInsets.all(3.0),
+      leading: new Icon(
+        Icons.whatshot,
+        color: color,
+        size: 40.0,
+      ),
+      title: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new Text(
+            name,
+            textAlign: TextAlign.start,
+            style: new TextStyle(color: UIData.blackOrWhite),
+            overflow: TextOverflow.ellipsis,
+          ),
+          new Text(
+            "${game.date.day}/${game.date.month}/${game.date.year} ${game.time}",
+            style: new TextStyle(color: UIData.blackOrWhite),
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+      subtitle: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new Text(
+            "Placing: ${game.placing}/${game.playerAmount}",
+            textAlign: TextAlign.start,
+            style: new TextStyle(color: UIData.blackOrWhite),
+          ),
+          new Text(
+            "Profit: $profit$currency",
+            style: new TextStyle(color: UIData.blackOrWhite),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ProfilePageResults(
+                    user: widget.user,
+                    result: game,
+                    title: "Tournament",
+                  ))),
+    );
+  }
 }
